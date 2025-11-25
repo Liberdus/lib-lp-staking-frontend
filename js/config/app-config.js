aa2ec56293291a5fd6a6a683be9df8ac8a0f0f2a
dnlbui <dnlbui@proton.me>
2025-11-25 10:09:53 -0600

    Update staking contract addresses in app-config.js for Polygon networks

--- js/config/app-config.js
+++ js/config/app-config.js
@@ -25,7 +25,10 @@ window.CONFIG = {
                 decimals: 18
             },
             CONTRACTS: {
-                STAKING_CONTRACT: '0x3aca5B70C7153671Fb96705E9593DABd1Ff7717F'/* '0x5e4216c952190BEA7abC4Fc64e990AfbF4F9885a' */
+                STAKING_CONTRACT: '0x65198C2893a62043fEd14400567a57915b84B63F'
+                                  /* '0x5e4216c952190BEA7abC4Fc64e990AfbF4F9885a' */
+                                  /* '0x3aca5B70C7153671Fb96705E9593DABd1Ff7717F' */
+
             }
         },
         POLYGON_MAINNET: {
